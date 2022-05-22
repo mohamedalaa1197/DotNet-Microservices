99636a077da2082bd5e6bc14b724a368a5796c58
mohamedalaa1197 <mohamed.mohdy1197@gmail.com>
2022-05-22 21:35:14 +0200

    Configure DB and Seed with Data

---
+++ src/Microservices/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var DB = client.GetDatabase(config.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = DB.GetCollection<Product>(config.GetValue<string>("DatabaseSettings:CollectionName"));
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
