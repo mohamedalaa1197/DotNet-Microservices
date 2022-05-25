02f60edc03e929f39d9d876eca99a293e7516aa7
mohamedalaa1197 <mohamed.mohdy1197@gmail.com>
2022-05-25 23:14:16 +0200

    Adding Basket Ms and Configure Redis Cache

---
+++ src/Microservices/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+
+        }
+        public ShoppingCart(string UserName)
+        {
+
+        }
+        public string UserName { get; set; }
+        public ICollection<ShoppingCartItem> Items { get; set; }
+
+
+        private decimal TotalPrice(ICollection<ShoppingCartItem> items)
+        {
+            decimal totalPrice = 0;
+            foreach (var item in items)
+            {
+                totalPrice += item.Price * item.Quantity;
+            }
+            return totalPrice;
+        }
+    }
+
+
+}
---
+++ src/Microservices/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
