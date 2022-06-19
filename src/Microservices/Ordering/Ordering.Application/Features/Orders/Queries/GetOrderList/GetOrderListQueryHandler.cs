8c7b8920614c75c74a3bb7162452a78aec86817c
mohamedalaa1197 <mohamed.mohdy1197@gmail.com>
2022-06-19 20:08:57 +0200

    Implementing CQRS Implementation

---
+++ src/Microservices/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrderListQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Presistence;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrderListQueryHandler : IRequestHandler<GetOrderListQuery, List<OrdersVM>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrderListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<OrdersVM>> Handle(GetOrderListQuery request, CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrderByUserName(request.UserName);
+            return _mapper.Map<List<OrdersVM>>(orderList);
+        }
+    }
+}
