5883bac5eb3fda13b8a38388e9f16876a7a024e1
mohamedalaa1197 <mohamed.mohdy1197@gmail.com>
2022-08-07 16:43:43 +0200

    Adding the client Application and configure it

---
+++ src/WebApps/AspnetRunBasics/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+namespace Shopping.Aggregator.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+                throw new ApplicationException($"Something went wrong when calling API: {response.ReasonPhrase}");
+
+            var dataString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<T>(dataString,
+                new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+
+        public static Task<HttpResponseMessage> PostAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            return httpClient.PostAsync(url, content);
+        }
+
+        public static Task<HttpResponseMessage> PutAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            return httpClient.PutAsync(url, content);
+        }
+    }
+}
---
+++ src/WebApps/AspnetRunBasics/Services/BasketService.cs
@@ -0,0 +1,30 @@
+﻿using AspnetRunBasics.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client)
+        {
+            _client = client;
+        }
+        public Task CheckoutBasket(BasketCheckoutModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<BasketModel> GetBasket(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<BasketModel> UpdateBasket(BasketModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
